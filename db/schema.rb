9c81a241f25516a6f629629c6792fea8cce1b025
Megha Bellary <Megha@Vishwanaths-MBP.fios-router.home>
2018-05-05 17:23:12 -0400

    api endpoints: movies list and movie show, database:movies table and seed data, models: movie

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_02_165937) do
+ActiveRecord::Schema.define(version: 2018_05_05_164835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "plot", null: false
+    t.date "release_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
